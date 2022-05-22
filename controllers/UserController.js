839e338833c3cc8b73eff8f0ae23409f9a4e19b0
HedgehogKU <ccc908925@gmail.com>
2022-05-22 16:07:37 +0800

    FIX: remove try catch in UserController

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,48 +6,40 @@ const success = require('../service/responseSuccess');
 
 module.exports = {
     async getUser(req, res, next) {
-        try {
-            const { id } = req.params;
-            const data = await UserModel.findById(id, 'name avatar');
-            success(res, data);
-        } catch(err) {
-            next(err);
-        }
+        const { id } = req.params;
+        const data = await UserModel.findById(id, 'name avatar');
+        success(res, data);
     },
     async insertUser(req, res, next) {
-        try {
-            const data = req.body;
-            const {
-                name,
-                sex,
-                email,
-                password,
-            } = data;
+        const data = req.body;
+        const {
+            name,
+            sex,
+            email,
+            password,
+        } = data;
 
-            if ( !name ) {
-                return appError('【暱稱】必填', next);
-            }
-
-            if ( !sex ) {
-                return appError('【性別】必填', next);
-            }
-
-            if ( !email ) {
-                return appError('【帳號】必填', next);
-            }
-
-            if ( !password ) {
-                return appError('【密碼】必填', next);
-            }
-
-            data.password = await bcrypt.hash(data.password, 12);
-
-            const result = await UserModel.create(data);
-            result.password = undefined;
-
-            success(res, result, 201);
-        } catch(err) {
-            next(err);
+        if ( !name ) {
+            return appError('【暱稱】必填', next);
         }
+
+        if ( !sex ) {
+            return appError('【性別】必填', next);
+        }
+
+        if ( !email ) {
+            return appError('【帳號】必填', next);
+        }
+
+        if ( !password ) {
+            return appError('【密碼】必填', next);
+        }
+
+        data.password = await bcrypt.hash(data.password, 12);
+
+        const result = await UserModel.create(data);
+        result.password = undefined;
+
+        success(res, result, 201);
     }
 }
